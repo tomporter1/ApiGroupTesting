34da551ab4ac684c51bcebe0bb261cb5cacd09e6
thamish perera <thamishperera24@yahoo.co.uk>
2020-07-13 15:12:01 +0100

    Initial Commit

---
+++ JplApiTesting/JplApiTesting/ApiObjectModels/Scout/HTTPManager/ScoutLatestCallManager.cs
@@ -0,0 +1,22 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+
+namespace JplApiTesting.ApiObjectModels.Scout.HTTPManager
+{
+    public class ScoutLatestCallManager
+    {
+        private readonly IRestClient _client;
+        private IRestResponse _response;
+
+        public ScoutLatestCallManager()
+        {
+
+        }
+
+        internal string GetAllCadData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JplApiTesting/JplApiTesting/ApiObjectModels/Scout/Services/ScoutService.cs
@@ -0,0 +1,14 @@
+﻿using JplApiTesting.ApiObjectModels.Scout.DataHandling;
+using JplApiTesting.ApiObjectModels.Scout.HTTPManager;
+using Newtonsoft.Json.Linq;
+
+namespace JplApiTesting.ApiObjectModels.Scout.Services
+{
+    public class ScoutService
+    {
+        public ScoutLatestCallManager callManager = new ScoutLatestCallManager();
+        public ScoutLatestDTO dto = new ScoutLatestDTO();
+        public string liveCurrent;
+        public JObject json_current;
+    }
+}
