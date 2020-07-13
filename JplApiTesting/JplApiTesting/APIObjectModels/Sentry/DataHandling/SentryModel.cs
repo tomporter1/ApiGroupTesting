bec91682c45d6dd857ed5328c4a3134600cf8875
Sunny-Sahota <63403015+Sunny-Sahota@users.noreply.github.com>
2020-07-13 14:46:00 +0100

    Basic Class structure

---
+++ JplApiTesting/JplApiTesting/APIObjectModels/Sentry/DataHandling/SentryModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JplApiTesting.ApiObjectModels.Sentry.DataHandling
+{
+	class SentryModel
+	{
+	}
+}
