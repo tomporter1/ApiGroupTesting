466bc6c3050f093a6991ab70581c3fbe6fbc8a4f
Tom Porter <tomporter62@gmail.com>
2020-07-14 09:18:15 +0100

    Added super config reader class

--- JplApiTesting/JplApiTesting/ApiObjectModels/CAD/CADConfigReader.cs
+++ JplApiTesting/JplApiTesting/ApiObjectModels/CAD/CADConfigReader.cs
@@ -1,14 +1,7 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.IO;
-
-namespace JplApiTesting.ApiObjectModels.CAD
+﻿namespace JplApiTesting.ApiObjectModels.CAD
 {
-    public class CADConfigReader
+    public class CADConfigReader : ConfigReader
     {
-        private static ConfigRoot _configObj => JsonConvert.DeserializeObject<ConfigRoot>(File.ReadAllText(_jsonFilePath));
-        private static readonly string _jsonFilePath = AppDomain.CurrentDomain.BaseDirectory + @"ApiObjectModels\Config.json";
-    
-        public static readonly string BaseUrl = _configObj.CAD.url;
+          public static readonly string BaseUrl = _configObj.CAD.url;
     }
 }
