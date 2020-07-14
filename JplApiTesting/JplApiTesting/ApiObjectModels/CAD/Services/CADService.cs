dbdf0036bcc2367c8a01088cd6f7cb9565a45959
Tom Porter <tomporter62@gmail.com>
2020-07-14 12:36:22 +0100

    Made cad service abstract

--- JplApiTesting/JplApiTesting/ApiObjectModels/CAD/Services/CADService.cs
+++ JplApiTesting/JplApiTesting/ApiObjectModels/CAD/Services/CADService.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 namespace JplApiTesting.ApiObjectModels.CAD.Services
 {
     //https://ssd-api.jpl.nasa.gov/doc/cad.html
-    public class CADService
+    public abstract class CADService
     {
         public CadLatestCallManager callManager = new CadLatestCallManager();
         public CadLatestDTO dto = new CadLatestDTO();
