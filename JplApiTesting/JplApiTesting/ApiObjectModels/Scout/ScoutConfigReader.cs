9f63ad9ff57959f16413e04e3cc0dd47f01c99dc
Tom Porter <tomporter62@gmail.com>
2020-07-14 10:47:51 +0100

    Changed all config readers to inherit from a base class

--- JplApiTesting/JplApiTesting/ApiObjectModels/Scout/ScoutConfigReader.cs
+++ JplApiTesting/JplApiTesting/ApiObjectModels/Scout/ScoutConfigReader.cs
@@ -1,20 +1,7 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using JplApiTesting.ApiObjectModels.Scout;
-
-namespace JplApiTesting.ApiObjectModels
+﻿namespace JplApiTesting.ApiObjectModels
 {
-    public class ScoutConfigReader
+    public class ScoutConfigReader: ConfigReader
     {
-        private static ConfigRoot json => JsonConvert.DeserializeObject<ConfigRoot>(File.ReadAllText(jsonFilePath));
-        private static readonly string jsonFilePath = AppDomain.CurrentDomain.BaseDirectory +  @"ApiObjectModels\Config.json";
-        public static readonly string ScoutUrl = json.Scout.url;
+        public static readonly string ScoutUrl = _configObj.Scout.url;
     }
 }
