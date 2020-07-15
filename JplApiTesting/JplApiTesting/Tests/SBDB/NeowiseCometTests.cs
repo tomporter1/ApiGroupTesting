252d4a056a85b5e857cc6653eeb305f4a8cb7d32
thamish perera <thamishperera24@yahoo.co.uk>
2020-07-15 15:50:40 +0100

    Added Tests to SBDB Api

--- JplApiTesting/JplApiTesting/Tests/SBDB/NeowiseCometTests.cs
+++ JplApiTesting/JplApiTesting/Tests/SBDB/NeowiseCometTests.cs
@@ -11,20 +11,66 @@ namespace JplApiTesting.Tests.SBDB
 {
     public class NeowiseCometTests
     {
-        private SbdbSpecificSmallBodyService _cadService;
+        private SbdbSpecificSmallBodyService _sbdbService;
         private readonly string _bodyName = "2020 F3";
 
         [OneTimeSetUp]
         public void OneTimeSetup()
         {
-            _cadService = new SbdbSpecificSmallBodyService(_bodyName);
+            _sbdbService = new SbdbSpecificSmallBodyService(_bodyName);
         }
 
         [Test]
         [Author("T Porter")]
         public void CallingTheAPI_ReturnsCorrectComet()
         {
-            Assert.That(_cadService.DTO.SbdbInfo._object.fullname, Is.EqualTo("C/2020 F3 (NEOWISE)"));
+            Assert.That(_sbdbService.DTO.SbdbInfo._object.fullname, Is.EqualTo("C/2020 F3 (NEOWISE)"));
+        }
+
+        [Test]
+        [Author("T Perera")]
+        public void CallingTheAPI_ReturnsCorrectSource()
+        {
+            Assert.That(_sbdbService.DTO.SbdbInfo.signature.source, Is.EqualTo("NASA/JPL Small-Body Database (SBDB) API"));
+        }
+
+        [Test]
+        [Author("T Perera")]
+        public void CallingTheAPI_ReturnsCorrectFirstObservation() 
+        {
+            Assert.That(_sbdbService.DTO.SbdbInfo.orbit.first_obs, Is.EqualTo("2020-03-27"));
+        }
+
+        [Test]
+        [Author("T Perera")]
+        public void CallingTheAPI_ReturnsCorrectOrbitProducer()
+        {
+            Assert.That(_sbdbService.DTO.SbdbInfo.orbit.producer, Is.EqualTo("Otto Matic"));
+        }
+
+        [Test]
+        [Author("T Perera")]
+        public void CallingTheAPI_ReturnsCorrectSPKID()
+        {
+            Assert.That(_sbdbService.DTO.SbdbInfo._object.spkid, Is.EqualTo("1003667"));
+        }
+
+        [TestCase("Content-Type", "application/json")]
+        [TestCase("Transfer-Encoding", "chunked")]
+        [TestCase("Connection", "keep-alive")]
+        [TestCase("Server", "nginx")]
+        [Author("T Perera")]
+        public void CheckReturnsCorrectHeader(string key, string expected)
+        {
+            Assert.That(_sbdbService.callManager.GetContentTypeHeader()[key], Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Author("T Perera")]
+        public void CheckHeaderReturnsCorrectDate()
+        {
+            string date = DateTime.Now.AddHours(-1).ToString("r").Remove(22, 7);
+            Assert.That(_sbdbService.callManager.GetContentTypeHeader()["Date"], Does.Contain(date));
         }
     }
 }
