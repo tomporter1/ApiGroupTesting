87f37f40141b505771cf6e0c7e1cf154633776c4
Tom Porter <tomporter62@gmail.com>
2020-07-15 15:06:59 +0100

    Added framework for the SBDB API that can get information about a specific body (comet) in space

---
+++ JplApiTesting/JplApiTesting/ApiObjectModels/SBDB/HTTPManager/SbdbCallManager.cs
@@ -0,0 +1,14 @@
+﻿using RestSharp;
+
+namespace JplApiTesting.ApiObjectModels.SBDB.HTTPManager
+{
+    public class SbdbCallManager : BaseCallManager
+    {
+        public SbdbCallManager()
+        {
+            _client = new RestClient(SbdbConfigReader.BaseUrl);
+        }
+
+        internal string GetSpecificBodyData(string bodyName) => CreateGetRequest($"?sstr={bodyName}");
+    }
+}
---
+++ JplApiTesting/JplApiTesting/ApiObjectModels/SBDB/Services/SbdbService.cs
@@ -0,0 +1,19 @@
+﻿using JplApiTesting.ApiObjectModels.SBDB.DataHandiling;
+using JplApiTesting.ApiObjectModels.SBDB.HTTPManager;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JplApiTesting.ApiObjectModels.SBDB.Services
+{
+    public class SbdbService : ServiceBase
+    {
+        public SbdbCallManager callManager = new SbdbCallManager();
+        public SbdbDTO DTO = new SbdbDTO();
+
+        protected void SetUp()
+        {
+            DTO.DeserializeLatestSBDB(ResponceData);
+            JObjectResponce = JsonConvert.DeserializeObject<JObject>(ResponceData);
+        }
+    }
+}
