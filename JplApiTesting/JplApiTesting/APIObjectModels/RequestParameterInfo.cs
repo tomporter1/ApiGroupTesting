93c376644ed3aee3d1d2c14d10db4dfec12f72d1
Tom Porter <tomporter62@gmail.com>
2020-07-16 15:05:51 +0100

    Removed all code from old project

--- JplApiTesting/JplApiTesting/APIObjectModels/RequestParameterInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace JplApiTesting.ApiObjectModels
-{
-    public struct RequestParameterInfo
-    {
-        public string Label { get; set; }
-        public string Data { get; set; }
-    }
-}
